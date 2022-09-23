26e6d01e92f022ed6a7e238e9141f2929df09102
Zach Wynne <s1879083@ed.ac.uk>
2022-09-23 11:18:54 +0100

    Minor edits + favicon addition

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,6 +31,6 @@ templates_path = ['_templates']
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
-
+html_favicon = 'uoe.ico'
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
